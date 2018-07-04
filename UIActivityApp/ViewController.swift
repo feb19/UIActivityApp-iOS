bd6e64b15fd601bdec3736cf54ee2d85f2538812
Nobuhiro Takahashi <nt@feb19.jp>
2018-07-05 07:55:01 +0900

    Initial Commit

---
+++ UIActivityApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIActivityApp
+//
+//  Created by Nobuhiro Takahashi on 2018/07/05.
+//  Copyright © 2018年 Nobuhiro Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
